eb363fc47b57f3aba75fadd3988c83874e007ce9
Niranjan B <niranjannb7777@gmail.com>
2023-07-26 23:38:08 +0530

    [Main] Added Last Update

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
+import datetime
+
 from jinja2 import Environment, FileSystemLoader
 
 from utils.articles import get_articles
 from utils.handles import get_handles
 
 data = {'job': 'Software Developer/Backend Developer', 'place': 'Kerala, India', 'header': 'images/header.jpeg',
-        'profile_url': 'https://github.com/niranjanneeru', 'handles': get_handles(), 'articles': get_articles()}
+        'profile_url': 'https://github.com/niranjanneeru', 'handles': get_handles(), 'articles': get_articles(),
+        'date': datetime.date.today().strftime("%d %B %Y")}
 
 environment = Environment(loader=FileSystemLoader("templates/"))
 template = environment.get_template("main.jinja")
